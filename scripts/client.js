9652f00a300de2d934091afb379c859dab366936
Jonathon Baxendell <j.bax626@gmail.com>
2022-07-18 11:41:56 -0500

    calculate monthly total funciton added

--- scripts/client.js
+++ scripts/client.js
@@ -4,14 +4,22 @@ $(readyNow);
 
 function readyNow() {
     // console.log('readyNOw')
-    // Event delegator
+
+    // Event delegators
     $('body').on('click', '.deleteBtn', deleteInput);
+
     // Event handlers
     $('#submit').on('click', displayInputs);
 }
 
-function displayInputs() {
+let emp = [];
+let monthly = 0;
 
+
+
+function displayInputs() {
+    
+    
     let firstName = $('#f-name').val();
     let lastName = $('#l-name').val();
     let ids = $('#emp-id').val();
@@ -19,13 +27,35 @@ function displayInputs() {
     let annualSal = $('#salary').val();
 
 
+    let newObj = {
+        first: firstName,
+        last: lastName,
+        annSal: annualSal
+    }
+
+    emp.push(newObj);
+
+    monthlyCalculation(emp);
+
+    console.log("monthly:", monthly);
+
+    
+
+    // console.log(monthly);
+    
+   
+
+    
+
     $('#data-table').append(`
         <tr>
             <td>${firstName}</td>
             <td>${lastName}</td>
             <td>${ids}</td>
             <td>${title}</td>
-            <td>${annualSal}</td>
+            <td> 
+                <span class="annual-sal">$${annualSal}</span>
+            </td>
             <td>
                 <button class="deleteBtn">Delete</button>
             </td>
@@ -37,8 +67,39 @@ function displayInputs() {
     $('#emp-id').val('');
     $('#emp-title').val('');
     $('#salary').val('');
+
+    $('#total').text(`
+        Total Monthly: $${Math.ceil(monthly)}
+    `)
+    // monthlyCalculation();
 }
+
+
 
 function deleteInput() {
     $(this).parent().parent().remove();
 }
+
+function monthlyCalculation() {
+    // console.log('in monthlyCalculation');
+    let sum = 0;
+    for(let i = 0; i < emp.length; i++){
+        sum += parseInt(emp[i].annSal);
+        // console.log(monthly);
+        
+        // console.log(sum);
+        // monthly = (numSum /= 12);
+    }
+    
+    monthly = sum /= 12;
+    // let str = monthly.Intl.NumberFormat('en-US');
+
+    // Math.ceil(monthly);
+
+    if(monthly > 20000){
+        $('.total-red').css('background-color', 'red');
+
+    }
+}
+
+console.log(monthlyCalculation());
